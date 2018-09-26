7ca5107f23619486065e4e789863bb16a6c155ab
deepak-7297 <34829049+deepak-7297@users.noreply.github.com>
2018-09-26 19:05:27 +0530

    Add files via upload

--- general.py
+++ general.py
@@ -26,22 +26,20 @@ def append_to_file(path, data):
         file.write(data + '\n')
 
 
-# Delete the contents of a file
+
 def delete_file_contents(path):
     open(path, 'w').close()
 
 
-# Read a file and convert each line to set items
 def file_to_set(file_name):
     results = set()
-    with open(file_name, 'rt') as f:
+    with open(file_name, 'rt',  encoding="utf8") as f:
         for line in f:
             results.add(line.replace('\n', ''))
     return results
 
 
-# Iterate through a set, each item will be a line in a file
 def set_to_file(links, file_name):
-    with open(file_name,"w") as f:
+    with open(file_name, "w", encoding="utf-8") as f:
         for l in sorted(links):
             f.write(l+"\n")
